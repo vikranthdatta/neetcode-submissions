8e271d10813b4addaff58ed47e9e0fcf46093a49
vikranth <56351827+vikranthdatta@users.noreply.github.com>
2026-07-16 19:56:53 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int diff = target - nums[i];
+            if(mp.find(diff)!= mp.end()) return {mp[diff],i};
+            mp.insert({nums[i],i});
+        }
+        return {};
+    }
+};
